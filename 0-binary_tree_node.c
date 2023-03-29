6df24debba7631b0b44f3d3446eab80897f6d0fd
tomutanyi <94693960+tomutanyi@users.noreply.github.com>
2023-03-30 00:42:51 +0300

    Update 0-binary_tree_node.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,15 +9,15 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *newt;
+	binary_tree_t *new;
 
-	newt = malloc(sizeof(binary_tree_t));
+	new = malloc(sizeof(binary_tree_t));
 	if (new == NULL)
 		return (NULL);
-	newt->n = value;
-	newt->parent = parent;
-	newt->left = NULL;
-	newt->right = NULL;
+	new->n = value;
+	new->parent = parent;
+	new->left = NULL;
+	new->right = NULL;
 
-	return (newt);
+	return (new);
 }
