66ba964e866d2b7404260f48cf60e8e9d9534389
tomutanyi <94693960+tomutanyi@users.noreply.github.com>
2023-03-31 00:22:11 +0300

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -48,6 +48,7 @@ int binary_tree_is_full(const binary_tree_t *tree);
 int binary_tree_is_perfect(const binary_tree_t *tree);
 binary_tree_t *binary_tree_sibling(binary_tree_t *node);
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
