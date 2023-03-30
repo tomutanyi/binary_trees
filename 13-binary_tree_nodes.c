9a43d0c1953b17cd46a83f2c3575d380376c7399
tomutanyi <tomutanyi@gmail.com>
2023-03-30 10:48:36 -0700

    task 11-13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_nodes - counts the nodes .
+ * @tree: ptr to the root node .
+ * Return: If tree is NULL, the function must return 0 .
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t nodes = 0;
+
+	if (tree)
+	{
+		nodes += (tree->left || tree->right) ? 1 : 0;
+		nodes += binary_tree_nodes(tree->left);
+		nodes += binary_tree_nodes(tree->right);
+	}
+	return (nodes);
+}
+
